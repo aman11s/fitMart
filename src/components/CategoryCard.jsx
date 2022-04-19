7b56677624a3a739e85210eda1c3155d07465374
Aman Singh <reachout.amansingh@gmail.com>
2022-04-20 01:52:18 +0530

    feat : update backend category details

---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const CategoryCard = () => {
+  return (
+    <>
+      <div class="category-item radius-5 p-2">
+        <h2 class="h2 container-flex-center category-text">Whey Protein</h2>
+        <img
+          class="img-responsive"
+          src="https://fitmart-screens.netlify.app/assets/products/whey-protein/ON-Whey-Protein-5-lb.webp"
+          alt="whey-protein"
+        />
+      </div>
+    </>
+  );
+};
