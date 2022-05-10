143616fed0713fee5c622fca328a26f6d0676d7e
Aman Singh <reachout.amansingh@gmail.com>
2022-05-11 00:31:35 +0530

    feat : add user profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { useAuth } from "../../context";
+import "./Profile.css";
 
 export const Profile = () => {
-  return <h1>User profile Page</h1>;
+  const {
+    userData: { user },
+  } = useAuth();
+
+  const { firstName, lastName, email } = user;
+
+  return (
+    <div className="profile">
+      <h2 className="h2 mt-3">My profile</h2>
+
+      <div className="user-profile-card container-flex-y-center m-2 radius-5 p-3">
+        <img
+          src="https://colorant-ui.netlify.app/components/Images/avatar.png"
+          alt="avatar"
+          className="avatar avatar-md-size"
+        />
+        <h5 className="h5">
+          {" "}
+          <span>{firstName}</span> <span>{lastName}</span>{" "}
+        </h5>
+        <div className="p-2">{email}</div>
+        <button className="btn primary-error-btn">Logout</button>
+      </div>
+    </div>
+  );
 };
